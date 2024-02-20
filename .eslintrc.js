b92267b7112cd8666ec384acd37afefc00be5d8b
o_ER4 <yuxtuke11@gmail.com>
2024-02-21 08:18:44 +0900

    openAPI.ts側に加筆

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,6 @@ module.exports = {
     'vue/eqeqeq': 'error',
     '@typescript-eslint/consistent-type-imports': 'error',
     'vue/require-default-prop': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     'vue/block-lang': [
       'error',
       {
