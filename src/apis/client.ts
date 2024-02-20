a46e12fac25524f6758aa33406ea1c2e7f2be115
o_ER4 <yuxtuke11@gmail.com>
2024-02-21 07:05:14 +0900

    実装した

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -2,7 +2,7 @@ import 'whatwg-fetch';
 
 import { Fetcher } from 'openapi-typescript-fetch';
 
-import { components, paths } from './openAPI';
+import type { paths, components } from './openAPI';
 
 // declare fetcher for paths
 const fetcher = Fetcher.for<paths>();
@@ -15,46 +15,89 @@ fetcher.configure({
 });
 
 // create fetch operations
-export const findAPIByStatus = fetcher.path('/items').method('get').create();
 export const getItems = fetcher.path('/items').method('get').create();
 
-export const postItems = fetcher.path('/items').method('post').create();
+export const getItem = fetcher.path('/items/{itemId}').method('get').create();
 
-export const patchItems = fetcher
+export const deleteItem = fetcher
   .path('/items/{itemId}')
-  .method('patch')
+  .method('delete')
   .create();
 
-export const postBorrow = fetcher
-  .path('/items/{itemId}/borrowing/equipment')
-  .method('post')
+export const editItem = fetcher
+  .path('/items/{itemId}')
+  .method('parameters')
   .create();
 
 export const postComment = fetcher
   .path('/items/{itemId}/comments')
-  .method('post')
+  .method('parameters')
   .create();
-
-export const postFile = fetcher.path('/files').method('post').create();
-
-export const getFile = fetcher.path('/files').method('post').create();
 
 export const postItemOwners = fetcher
   .path('/items/{itemId}/owners')
   .method('post')
   .create();
 
-export const postOwnership = fetcher.path(
-  '/items/{itemId}/owners/{ownershipId}',
-);
+export const deleteItemOwners = fetcher
+  .path('/items/{itemId}/owners/{ownershipId}')
+  .method('parameters')
+  .create();
 
-export const postReturn = fetcher.path(
-  '/items/{itemId}/borrowing/equipment/return',
-);
+export const postBorrowEquipment = fetcher
+  .path('/items/{itemId}/borrowing/equipment')
+  .method('post')
+  .create();
 
-export const getBorrowingById = fetcher
+export const postBorrowEquipmentReturn = fetcher
+  .path('/items/{itemId}/borrowing/equipment/return')
+  .method('post')
+  .create();
+
+export const postBorrow = fetcher
   .path('/items/{itemId}/owners/{ownershipId}/borrowings')
   .method('post')
   .create();
 
-export const deleteItem = fetcher.path('/items/{itemId}').method('delete');
+export const getBorrowingById = fetcher
+  .path('/items/{itemId}/owners/{ownershipId}/borrowings/{borrowingId}')
+  .method('get')
+  .create();
+
+export const postBorrowReply = fetcher
+  .path('/items/{itemId}/owners/{ownershipId}/borrowings/{borrowingId}/reply')
+  .method('post')
+  .create();
+
+export const postReturn = fetcher
+  .path('/items/{itemId}/owners/{ownershipId}/borrowings/{borrowingId}/return')
+  .method('post')
+  .create();
+
+export const addLike = fetcher
+  .path('/items/{itemId}/likes')
+  .method('post')
+  .create();
+
+export const postFile = fetcher.path('/files').method('post').create();
+
+export const getFile = fetcher.path('/files/{fileId}').method('get').create();
+
+export type itemPosted = components['schemas']['itemPosted'];
+export type itemSummary = components['schemas']['itemSummary'];
+export type comment = components['schemas']['comment'];
+export type postComment = components['schemas']['postComment'];
+export type transaction = components['schemas']['transaction'];
+export type transactionEquipment =
+  components['schemas']['transactionEquipment'];
+export type borrowRequest = components['schemas']['borrowRequest'];
+export type borrowRequestEquipment =
+  components['schemas']['borrowRequestEquipment'];
+export type borrowing = components['schemas']['borrowing'];
+export type borrowReply = components['schemas']['itemPosted'];
+export type borrowReturn = components['schemas']['borrowReturn'];
+export type ownership = components['schemas']['ownership'];
+export type postOwnership = components['schemas']['postOwnership'];
+export type tag = components['schemas']['tag'];
+export type owner = components['schemas']['owner'];
+export type file = components['schemas']['file'];
