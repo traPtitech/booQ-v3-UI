2a8cb90e9965499ae4b9705c38cc6699b5a7cba6
o_ER4 <yuxtuke11@gmail.com>
2024-02-25 17:29:02 +0900

    main.tsに設定加筆した、今後ページ作成時にそのコンポーネントごとにimportする

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/primevue.min.css';
 import router from './router';
 import App from './App.vue';
 
@@ -7,6 +9,8 @@ import './index.scss';
 import '@purge-icons/generated';
 
 const app = createApp(App);
+
+app.use(PrimeVue);
 app.use(router);
 app.use(createPinia());
 app.mount('#app');
