4a29b0117d7fa847d40951cdf25b82b114d759cc
Natsuki <qq973313484@gmail.com>
2024-07-12 00:34:09 +0900

    基本的な枠組みの実装

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import type { RouteRecordRaw } from 'vue-router';
 import { createRouter, createWebHistory } from 'vue-router';
+import registerView from '@/pages/registerView.vue';
 
 export const routerHistory = createWebHistory();
 
@@ -8,6 +9,11 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     name: 'Dashboard',
     component: () => import('@/pages/DashBoard.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: registerView,
   },
 ];
 
---
+++ src/shared/components/RadioCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="$style.container">
+    <div :class="$style.title_wrapper">
+      <div :class="$style.title">選択肢</div>
+      <radio-button />
+    </div>
+    <div :class="$style.text">選択肢1の説明</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import RadioButton from 'primevue/radiobutton';
+
+const props = withDefaults(
+  defineProps<{
+    width?: string;
+  }>(),
+  {
+    width: '320px',
+  },
+);
+
+props.width;
+</script>
+
+<style lang="scss" module>
+.container {
+  width: 351px;
+  height: 112px;
+  padding: 16px;
+  font-size: 16px;
+  border: 1px solid $color-secondary;
+  border-radius: 4px;
+}
+
+.title_wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+
+.title {
+  color: $color-text-primary;
+  font-weight: bold;
+  margin-bottom: 8px;
+}
+
+.text {
+  color: $color-text-secondary;
+  font-size: 16px;
+}
+</style>
