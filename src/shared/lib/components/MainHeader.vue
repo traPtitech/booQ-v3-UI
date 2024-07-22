3e549c0765064fd304d435989223acf74c0a62f7
o_ER4 <yuxtuke11@gmail.com>
2024-07-22 12:10:26 +0900

    MainHeaderも修正した

--- src/shared/lib/components/MainHeader.vue
+++ src/shared/lib/components/MainHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="content">
-    <div class="header">
+    <div class="leftbox">
       <img
         src="/safari-pinned-tab.svg"
         alt="Safari Pinned Tab Icon"
         style="width: 40px; filter: brightness(70%)"
       />
+    </div>
+    <div class="rightbox">
       <ChipCard :label="ChipLabel" />
       <IconAvatar :name="UserName" />
     </div>
@@ -19,16 +21,19 @@ const UserName = 'o_ER4'; // 実際は指定のユーザーの名前を取得す
 const ChipLabel = '物品を追加'; //ボタンのラベルをpropsで使えるようにして使いまわす
 </script>
 
-<style lang="scss" module>
+<style lang="scss">
 .content {
   display: flex;
-  flex: auto;
-  flex-direction: column;
-  justify-content: flex-start;
+  align-items: center;
   justify-content: space-between;
 }
-.header {
+
+.leftbox {
+  margin-right: auto;
+}
+
+.rightbox {
   display: flex;
-  justify-content: space-between;
+  align-items: center;
 }
 </style>
