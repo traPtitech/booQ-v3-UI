8b8779341d7c8d34cb761fd51a6431dad70fcad1
soramea <soraquyia@gmail.com>
2024-12-23 18:56:18 +0900

    textinput の下地を実装

---
+++ src/shared/components/TextInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  placeholder: string;
+  size: number;
+}>();
+</script>
+
+<template>
+  <input
+    type="text"
+    class="text-input"
+    :size="props.size"
+    :placeholder="props.placeholder"
+  />
+</template>
+<style lang="scss" scoped>
+.text-input {
+  height: 24px;
+  border-radius: 4px;
+  border-color: $color-border;
+  border: 1px solid;
+}
+.text-input::placeholder {
+  font-size: 16px;
+  font-weight: 500;
+  color: $color-text-placeholder;
+}
+</style>
