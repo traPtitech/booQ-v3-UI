60aecdc0a493ea01d450bf5e79fba9136e963d4a
soramea <soraquyia@gmail.com>
2024-12-25 08:56:13 +0900

    add slot

--- src/shared/components/TextInput.vue
+++ src/shared/components/TextInput.vue
@@ -8,23 +8,39 @@ const props = defineProps<{
 </script>
 
 <template>
-  <input
-    type="text"
-    class="text-input"
-    :size="props.size"
-    :placeholder="props.placeholder"
-  />
+  <div class="background">
+    <slot name="left" />
+    <input
+      type="text"
+      class="text-input"
+      :size="props.size"
+      :placeholder="props.placeholder"
+    />
+    <slot name="right" />
+  </div>
 </template>
+
 <style lang="scss" scoped>
-.text-input {
-  height: 24px;
-  border-radius: 4px;
+.background {
+  display: inline flex;
+  gap: 8px;
+  height: 32px;
+  align-items: center;
+  padding: 8px 16px;
   border-color: $color-border;
   border: 1px solid;
+  border-radius: 4px;
 }
-.text-input::placeholder {
+.text-input {
+  height: 32px;
+  border: none;
+  outline: 0;
   font-size: 16px;
   font-weight: 500;
+  background-color: transparent;
+}
+.text-input::placeholder {
   color: $color-text-placeholder;
+  opacity: 1;
 }
 </style>
