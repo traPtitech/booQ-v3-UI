39e209261fca86abbd12bccaeaa1b44537b98b37
oER4 <yuxtuke11@gmail.com>
2025-06-10 23:58:50 +0900

    ホバー時に色が変わらないのを修正

--- src/shared/lib/components/ChipCard.vue
+++ src/shared/lib/components/ChipCard.vue
@@ -33,22 +33,36 @@ withDefaults(defineProps<Props>(), {
   &.color-primary {
     background-color: $color-primary;
     border-color: $color-primary;
+    color: $color-text-white;
+
+    &:hover {
+      background-color: $color-primary-hover;
+      border-color: $color-border-hover;
+      color: $color-primary;
+    }
   }
 
   &.color-error {
     background-color: $color-error;
     border-color: $color-error;
+    color: $color-text-white;
+
+    &:hover {
+      background-color: lighten($color-error, 10%);
+      border-color: darken($color-error, 10%);
+    }
   }
 
   &.color-secondary {
     background-color: $color-secondary;
     border-color: $color-secondary;
-  }
-}
-.buton-hover {
-  &:hover {
-    background-color: $color-primary;
-    border-color: $color-border-hover;
+    color: $color-text-secondary; // 文字色
+
+    &:hover {
+      background-color: $color-secondary-hover;
+      border-color: darken($color-secondary, 10%);
+      // 必要に応じてホバー時の文字色も指定
+    }
   }
 }
 </style>
