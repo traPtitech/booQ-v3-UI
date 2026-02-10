ab6877673122a5eb6b5856c22db501d1752bb852
cp-20 <n.naoki.1587@gmail.com>
2026-02-11 05:11:31 +0900

    remove purge-icons

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import router from './router';
-import App from './App.vue';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import router from "./router";
+import App from "./App.vue";
 
-import './styles/tailwind.css';
-import '@purge-icons/generated';
+import "./styles/tailwind.css";
 
 const app = createApp(App);
 
@@ -14,13 +13,13 @@ app.use(PrimeVue, {
   theme: {
     preset: Aura,
     options: {
-      prefix: 'p',
-      darkModeSelector: 'system',
-      cssLayer: { name: 'primevue' },
+      prefix: "p",
+      darkModeSelector: "system",
+      cssLayer: { name: "primevue" },
     },
   },
 });
 
 app.use(router);
 app.use(createPinia());
-app.mount('#app');
+app.mount("#app");
