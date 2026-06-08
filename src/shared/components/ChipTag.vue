f7e8f14fb77010f54acd2032c953f00ce6330dc1
oER4 <yuxtuke11@gmail.com>
2026-06-08 17:33:05 +0900

    Componentを復元したのでこれからprimevueのcomponentに変えてく

---
+++ src/shared/components/ChipTag.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Chip from 'primevue/chip';
+
+defineProps<{
+  label: string;
+}>();
+</script>
+<template>
+  <Chip class="p-chip">
+    <span>{{ label }}</span>
+  </Chip>
+</template>
---
+++ src/shared/components/TextInput.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+defineProps<{
+  placeholder?: string;
+  size: 'sm' | 'md' | 'lg';
+}>();
+</script>
+
+<template>
+  <div class="background">
+    <div v-if="$slots['left']">
+      <slot name="left" class="side-icon" />
+    </div>
+    <input
+      type="text"
+      class="text-input"
+      :size="size"
+      :placeholder="placeholder"
+    />
+    <div v-if="$slots['right']">
+      <slot name="right" class="side-icon" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.background {
+  display: inline-flex;
+  height: 32px;
+  align-items: center;
+  border: 1px solid var(--color-border);
+  border-radius: 4px;
+}
+
+.background:focus-within {
+  outline: 2px solid;
+  outline-color: var(--color-primary);
+}
+.text-input {
+  height: 32px;
+  padding: 8px 8px;
+  border: none;
+  outline: none;
+  font-size: 16px;
+  font-weight: 500;
+  background-color: transparent;
+}
+.text-input::placeholder {
+  color: var(--color-text-placeholder);
+}
+.side-icon {
+  display: flex;
+  padding: 0 4px;
+  height: 32px;
+  align-items: center;
+}
+</style>
